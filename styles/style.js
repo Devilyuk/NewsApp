dec78739447acfb144ac08bed3ee25064064fa15
Andrey <ankurenko@1cbit.ru>
2025-04-05 21:59:36 +0700

    Добавляет шрифты

---
+++ styles/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const gStyle = StyleSheet.create({
+    mainBox: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fafafa'
+    },
+    title: {
+        fontSize: 20,
+        color: '#333',
+        // fontFamily: ''
+    }
+})
